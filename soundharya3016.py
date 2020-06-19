130ac811125af0fdb66829ea534970b0743685d7
Ksoundharya <67107093+Ksoundharya@users.noreply.github.com>
2020-06-19 08:40:51 +0530

    Add files via upload

---
+++ soundharya3016.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 1:SIMPLE CALCULATIONS
+
+# ADDITION
+
+# In[1]:
+
+
+s=9
+v=7
+z=s+v
+print(z)
+
+
+# SUBTRACTION
+
+# In[2]:
+
+
+a=5
+b=4
+c=a-b
+print(c)
+
+
+# MULTIPLICATION
+
+# In[3]:
+
+
+x=45
+y=67
+z=x*y
+print(z)
+
+
+# DIVISION
+
+# In[4]:
+
+
+a=6
+b=78
+c=a/b
+print(c)
+
+
+# FLOOR DIVISION
+
+# In[5]:
+
+
+n=6
+m=5
+k=n//m
+print(k)
+
+
+# MODULES
+
+# In[6]:
+
+
+a=34
+b=67
+c=a%b
+print(c)
+
+
+# EXPONENT
+
+# In[9]:
+
+
+a=2**4
+print(a)
+
+
+
+# 2.SIMPLE INTEREST
+
+# In[15]:
+
+
+p=5
+t=6
+r=9
+si=(p*t*r)/100
+print(si)
+
+
+# 3.AREA OF CIRCLE
+
+# In[16]:
+
+
+PI=3.142
+R=3
+A=PI*R*R
+print(A)
+
+
+# 4.AREA OF TRIANGLE
+
+# In[18]:
+
+
+b=8
+h=5
+k=0.5*b*h
+print(k)
+
+
+# 5.CELSIUS TO FAHRENHEIT
+
+# In[21]:
+
+
+C=100
+f=(1.8*C)+32
+print(f)
+
+
+# 6.AREA OF RECTANGLE
+
+# In[22]:
+
+
+l=9
+b=7
+a=l*b
+print(a)
+
+
+# 7.perimeter of square
+
+# In[23]:
+
+
+a=8
+p=4*a
+print(p)
+
+
+# 8.circumference of circle
+
+# In[24]:
+
+
+r=7
+pi=3.142
+c=2*pi*r
+print(c)
+
+
+# 9.swapping of two numbers
+
+# In[25]:
+
+
+x=8
+y=9
+temp=x
+x=y
+y=temp
+print(x)
+print(y)
+
